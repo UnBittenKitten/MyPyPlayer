1984ae0accdefaae9bc72501711c5b1b6a0a546a
nathah3rnandez-dotcom <nathah3rnandez@gmail.com>
2025-11-21 17:40:11 -0600

    no deberia funcionar aaa bbb

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
 from app_window import AppWindow
 
+from classes.queue import Queue
+
 if __name__ == "__main__":
     app = AppWindow()
     app.mainloop()
+
+    q = Queue()
+    # q.add(10)
+    for i in range(10):
+        q.add(i)
+
+    q.print()
+
