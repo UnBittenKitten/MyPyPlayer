ee2cca6db27c3529bbd23644f7d704c283cdc1da
Roberto Escobar <ZULINOSITY>
2025-11-22 19:21:45 -0600

    merge branch 'main' into feat-metadata-implementation [merge.py]

--- src/main.py
+++ src/main.py
@@ -5,10 +5,3 @@ from classes.queue import Queue
 if __name__ == "__main__":
     app = AppWindow()
     app.mainloop()
-
-    q = Queue()
-    # q.add(10)
-    for i in range(10):
-        q.add(i)
-
-    q.print()
