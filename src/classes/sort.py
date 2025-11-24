c00d814de619c0f2a89440ad93813311fa086e32
UnBittenKitten <luisperez.210505@gmail.com>
2025-11-24 01:00:39 -0600

    Made sort.py importable

--- src/classes/sort.py
+++ src/classes/sort.py
@@ -31,26 +31,26 @@ def merge(left, right, key, reverse):
 
     return result + left[i:] + right[j:]
 
+if __name__ == "__main__":
+    # USage Examples:
+    words = ["banana", "Apple", "grape", "orange"]
+    sorted_words = merge_sort(words, key=str.lower)
+    print(sorted_words)
 
-# USage Examples:
-words = ["banana", "Apple", "grape", "orange"]
-sorted_words = merge_sort(words, key=str.lower)
-print(sorted_words)
+    sorted_wordsr = merge_sort(words, key=str.lower, reverse=True)
+    print(sorted_wordsr)
 
-sorted_wordsr = merge_sort(words, key=str.lower, reverse=True)
-print(sorted_wordsr)
+    nums = [5, 2, 100, 23, 1]
+    print(merge_sort(nums))
 
-nums = [5, 2, 100, 23, 1]
-print(merge_sort(nums))
+    print(merge_sort(nums, reverse=True))
 
-print(merge_sort(nums, reverse=True))
+    people = [
+        {"name": "Carlos", "age": 30},
+        {"name": "Ana", "age": 20},
+        {"name": "Luis", "age": 25}
+    ]
 
-people = [
-    {"name": "Carlos", "age": 30},
-    {"name": "Ana", "age": 20},
-    {"name": "Luis", "age": 25}
-]
-
-sorted_people = merge_sort(people, key=lambda x: x["age"])
-print(sorted_people)
+    sorted_people = merge_sort(people, key=lambda x: x["age"])
+    print(sorted_people)
 
